d8a9d73e4a77169e9812a33cf8f979d14ec19bd7
Giorgi Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-01-11 14:08:56 +0400

    added move to markers logic onclicks method

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 class Workout {
     date = new Date()
     id = (Date.now() + '').slice(-10)
+    clicks = 0
 
     constructor(coords, distance, duration) {
         this.coords = coords // [lat,lng]
@@ -17,6 +18,10 @@ class Workout {
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(
             1,
         )} on ${months[this.date.getMonth()]} ${this.date.getDate()}`
+    }
+
+    click() {
+        this.clicks++
     }
 }
 
@@ -44,6 +49,7 @@ class Cycling extends Workout {
         super(coords, distance, duration)
         this.elevationGain = elevationGain
         this.calcSpeed()
+        this._setDescription()
     }
 
     calcSpeed() {
@@ -69,6 +75,7 @@ const inputElevation = document.querySelector('.form__input--elevation')
 
 class App {
     #map
+    #mapZoomLevel = 13
     #mapEvent
     #workouts = []
 
@@ -76,6 +83,10 @@ class App {
         this._getPosition()
         form.addEventListener('submit', this._newWorkout.bind(this))
         inputType.addEventListener('change', this._toggleElevationFields)
+        containerWorkouts.addEventListener(
+            'click',
+            this._moveToPopUp.bind(this),
+        )
     }
 
     _getPosition() {
@@ -93,7 +104,7 @@ class App {
         const { latitude, longitude } = position.coords
         const coords = [latitude, longitude]
 
-        this.#map = L.map('map').setView(coords, 14)
+        this.#map = L.map('map').setView(coords, this.#mapZoomLevel)
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution:
@@ -113,7 +124,7 @@ class App {
     _hideForm() {
         // Empty Inputs
         // prettier-ignore
-        inputDistance.value = inputDuration.value = inputCadence.value =inputElevation.value = ''
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
 
         form.style.display = 'none'
         form.classList.add('hidden')
@@ -243,6 +254,23 @@ class App {
             </li>
             `
         form.insertAdjacentHTML('afterend', html)
+    }
+
+    _moveToPopUp(e) {
+        const workoutEl = e.target.closest('.workout')
+
+        if (!workoutEl) return
+        const workout = this.#workouts.find(
+            (work) => work.id === workoutEl.dataset.id,
+        )
+        this.#map.setView(workout.coords, this.#mapZoomLevel, {
+            animate: true,
+            pan: {
+                duration: 1,
+            },
+        })
+
+        workout.click()
     }
 }
 
